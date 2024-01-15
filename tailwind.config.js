f19a6ca16b187f34c301fe086447604a3d943161
sabah-busthan <156265947+sabah-busthan@users.noreply.github.com>
2024-01-15 14:15:39 +0530

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: ["./dist/*.{html, js}"],
   theme: {
     extend: {
-      fontFamily: {
-        'noto': ['Noto Sans Malayalam', 'sans-serif'],
-        'manjari': ['manjari', 'sans-serif']
-      }
     },
   },
   plugins: [],
